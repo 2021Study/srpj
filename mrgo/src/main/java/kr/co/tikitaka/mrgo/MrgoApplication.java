ee124b0f75ccb2ff3720c64a347f80024c4c4c84
silverstar03 <eunbyoul0421@naver.com>
2021-04-18 00:35:17 +0900

    React와 Spring 연동

---
+++ mrgo/src/main/java/kr/co/tikitaka/mrgo/MrgoApplication.java
@@ -0,0 +1,13 @@
+package kr.co.tikitaka.mrgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrgoApplication.class, args);
+    }
+
+}
